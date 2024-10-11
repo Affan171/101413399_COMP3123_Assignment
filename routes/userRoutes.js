75951b704d9a1c4ebb9017e8121ece92995b94bc
Affan <Affanshaikh76112@gmail.com>
2024-10-10 22:27:12 -0400

    User Login route created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -32,5 +32,29 @@ router.post("/signup", async (req, res) => {
     }
 });
 
+// User Login
+router.post('/login', async(req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        // Check if the user exists
+        const user = await User.findOne({email});
+
+        if (!user) {
+            return res.status(400).json("Invalid email or password");
+        }
+
+        // Check if the password is correct
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json("Invalid email or password");
+        }
+
+        res.status(200).json({message: 'Login successful', user});
+    } catch (error) {
+        res.status(500).json({message: 'Internal Server Error', error});
+    }
+});
+
 // Export the module
 module.exports = router;
