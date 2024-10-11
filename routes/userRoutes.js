2ba6fc4aca3a767c4d5f9e35c3858972eb240dab
Affan <Affanshaikh76112@gmail.com>
2024-10-11 17:18:06 -0400

    Data Validation added in user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,19 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user'); // Import the user model
 const bcrypt = require('bcryptjs'); // For hashing the password
+const {body, validationResult} = require('express-validator');
 
 // User Signup
-router.post("/signup", async (req, res) => {
+router.post("/signup",[
+  // Data Validation:
+  body('username').notEmpty().withMessage('Username is required'),
+  body('email').isEmail().withMessage('Email must be valid').notEmpty().withMessage('Email is required'),
+  body('password').isLength({min : 6}).withMessage('Password must be atleast 6 characters long').isEmpty().withMessage('Password is required')  
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
     const { username, email, password } = req.body;
     
     try {
