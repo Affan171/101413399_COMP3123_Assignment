a72119c5cf766f9f173458fd67fc98926105a381
Affan <Affanshaikh76112@gmail.com>
2024-10-12 15:23:19 -0400

    app.js updated

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ const app = express();
 app.use(express.json());
 
 // MongoDB Connection
-const MONGO_URI = "mongodb+srv://Affan:Affskh%3F12345@cluster0.iqck3.mongodb.net/Assignment1?retryWrites=true&w=majority&appName=Cluster0";
+// const MONGO_URI = "mongodb+srv://Affan:Affskh%3F12345@cluster0.iqck3.mongodb.net/Assignment1?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.connect(MONGO_URI).then(() => {
+const MONGO_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
   console.log('Connected to MongoDB');
 }).catch((err) => {
   console.log('Failed to connect to MongoDB', err);
