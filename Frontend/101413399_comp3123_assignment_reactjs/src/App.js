1dceab5797382e91f9a25a2625c1166a3a165702
Affan <Affanshaikh76112@gmail.com>
2024-11-30 23:20:20 -0500

    Routes added in App.js

--- Frontend/101413399_comp3123_assignment_reactjs/src/App.js
+++ Frontend/101413399_comp3123_assignment_reactjs/src/App.js
@@ -1,14 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Logout from './components/Logout';
 import EmployeeList from './components/Employee/EmployeeList';
+import ProtectedRoute from './components/ProtectedRoute';
 
-function App() {
-  return (
-    <div className="App">
-      <EmployeeList />
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/logout" element={<Logout />} />
+                <Route
+                    path="/employees"
+                    element={
+                        <ProtectedRoute>
+                            <EmployeeList />
+                        </ProtectedRoute>
+                    }
+                />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
-
