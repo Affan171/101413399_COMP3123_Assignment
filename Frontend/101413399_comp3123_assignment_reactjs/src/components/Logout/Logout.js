61aa4a5f1a636a747b49056f3df629f6312743af
Affan <Affanshaikh76112@gmail.com>
2024-11-30 23:18:46 -0500

    ProtectedRoute component created

---
+++ Frontend/101413399_comp3123_assignment_reactjs/src/components/Logout/Logout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { logout } from '../services/authService';
+
+const Logout = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout(); // Clear token
+        navigate('/login'); // Redirect to login page
+    };
+
+    return (
+        <button onClick={handleLogout}>
+            Logout
+        </button>
+    );
+};
+
+export default Logout;
